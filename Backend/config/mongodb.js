d416c6b9b47f3d809382434bc0c6d167fcc5e184
Chandan Pandey <133705046+angry0batman@users.noreply.github.com>
2025-01-09 21:33:50 +0530

    database connection checked

--- Backend/config/mongodb.js
+++ Backend/config/mongodb.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const connectDB = async()=>{
 
     mongoose.connection.on('connected',() =>{
-        console.log(`Mongoose connected to ${process.env.MONGODB_URI}`);
+        console.log(`Database connected`);
     })
 
      await mongoose.connect(`${process.env.MONGODB_URI}`);
